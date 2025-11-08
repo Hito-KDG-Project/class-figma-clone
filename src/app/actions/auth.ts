fc2a283a82f3967615f62f8ae7881678213da8ef
apple-computer-machintosh <hirohito.tanaka1120@gmail.com>
2025-11-08 13:33:49 +0900

    signupの見た目の作成

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import bcrypt from "bcryptjs";
 import { signupSchema } from "schema";
+import { ZodError } from "zod";
+import { db } from "~/server/db";
 
 export async function register(
   prevSate: string | undefined,
@@ -11,7 +14,29 @@ export async function register(
       email: formData.get("email"),
       password: formData.get("password"),
     });
+
+    const user = await db.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return "ユーザーがすでに登録されています。";
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+      data: {
+        email: email,
+        password: hash,
+      },
+    });
   } catch (e) {
+    if (e instanceof ZodError) {
+      e.errors.map((error) => error.message).join(",");
+    }
     return "error";
   }
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,61 @@
+"use client";
+
+import Link from "next/link";
+import { useActionState } from "react";
+import { register } from "../actions/auth";
+
 export default function Page() {
-  return <p>Sign up これは日本語です</p>;
+  const [errorMessage, formAction, isPending] = useActionState(
+    register,
+    undefined,
+  );
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="w-full max-w-sm space-y-8 rounded-2xl border border-gray-300 px-6 py-14 pb-20">
+        <h1 className="text-2xl font-bold text-gray-700">アカウントの登録</h1>
+        <form action={formAction} className="space-y-4">
+          <div className="relative flex h-full flex-col space-y-2">
+            <label
+              htmlFor="email"
+              className="absolute top-2 left-4 text-[12px] text-gray-600"
+            >
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="h-16 rounded-md border border-gray-300 px-4 pt-4 focus:border-blue-600 focus:ring"
+              required
+            />
+          </div>
+          <div className="relative flex h-full flex-col space-y-2">
+            <label
+              htmlFor="email"
+              className="absolute top-2 left-4 text-[12px] text-gray-600"
+            >
+              パスワード
+            </label>
+            <input
+              id="email"
+              type="password"
+              className="h-16 rounded-md border border-gray-300 px-4 pt-4 focus:border-blue-600 focus:ring"
+              required
+            />
+          </div>
+          <button className="disabled:bg-gray300 w-full cursor-pointer rounded-md bg-blue-600 py-3 text-white transition-colors duration-300 ease-in-out hover:bg-blue-700 disabled:cursor-not-allowed">
+            登録
+          </button>
+          <p className="text-xs text-gray-500">
+            アカウントをお持ちの方へ {""}
+            <Link href="/signin" className="text-blue-600 underline">
+              ログイン
+            </Link>
+          </p>
+          {errorMessage && (
+            <p className="text-red500 text-sm">{errorMessage}</p>
+          )}
+        </form>
+      </div>
+    </div>
+  );
 }
